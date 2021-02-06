45b900c6fb2b823b79c838ffa085d1a1257b52ce
dg0397 <rgg0397@gmail.com>
2021-02-06 12:49:17 -0400

    1.6: unicafe step1

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({data}) => {
+  const {title} = data;
+  return(
+    <h1>{title}</h1>
+  )
+}
+
+const Button = ({handleClick,title}) => {
+  return(
+    <button onClick = {handleClick}>{title}</button>
+  )
+}
+
+const Statistics = ({good,neutral,bad}) => {
+  return(
+    <>
+      <h2>Statistics</h2>
+      <p>Good:{good}</p>
+      <p>Neutral:{neutral}</p>
+      <p>Bad:{bad}</p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const data = {
+    title : "Give Feedback",
+    category : ['good','neutral','bad']
+  }
+
+  const handlerClicks = (btnType) => {
+    if(btnType === 'good'){
+      return (() => setGood(good + 1))
+    }else if(btnType === 'neutral'){
+      return () => setNeutral(neutral + 1)
+    }else{
+      return () => setBad(bad + 1)
+    }
+  }
+
+  return (
+    <div>
+      <Header data = {data}/>
+      {
+        data.category.map(btn =>{
+          return(
+            <Button key = {btn} title = {btn} handleClick = {handlerClicks(btn)} />
+          )
+        })
+      }
+      <Statistics
+        good = {good} 
+        neutral = {neutral} 
+        bad = {bad} 
+      />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
