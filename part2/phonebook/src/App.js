11fb834b3ed30e10f1cafc6c41c1dbd62ddee101
dg0397 <rgg0397@gmail.com>
2021-02-12 19:48:19 -0400

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,51 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Person = ({name}) => {
-    return(
-        <p>{name}</p>
-    )
-}
+const Person = ({ name }) => {
+  return <p>{name}</p>;
+};
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
   const handleSubmit = (e) => {
-      e.preventDefault();
+    e.preventDefault();
+    if (persons.map((person) => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+    } else {
       const person = {
-          name: newName
-      }
-      setPersons(persons.concat(person))
-      setNewName("")
-  }
+        name: newName,
+      };
+      setPersons(persons.concat(person));
+      setNewName("");
+    }
+  };
 
   const handleChange = (e) => {
-      setNewName(e.target.value)
-  }
+    setNewName(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit = {handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input  value = {newName} onChange = {handleChange}/>
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {
-          persons.map(person => {
-              return(
-                  <Person key = {person.name} name = {person.name}/>
-              )
-          })
-      }
+      {persons.map((person) => {
+        return <Person key={person.name} name={person.name} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
